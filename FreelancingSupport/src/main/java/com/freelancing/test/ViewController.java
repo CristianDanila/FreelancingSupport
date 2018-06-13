f2dbe84fcf280be1603f5d210a3956434771eee6
C312922 <C312922@GUSDZQ050028.hd00.UNICREDITGROUP.EU>
2018-06-13 10:50:41 +0300

    Commit after mode changed

--- FreelancingSupport/src/main/java/com/freelancing/test/ViewController.java
+++ FreelancingSupport/src/main/java/com/freelancing/test/ViewController.java
@@ -1,14 +1,21 @@
 package com.freelancing.test;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
 
 	@RequestMapping("/")
-	public String index(){
+	public String index(Model model){
 		
+		model.addAttribute("datetime", new Date());
+		model.addAttribute("username", "Cristi");
+		model.addAttribute("mode", "production");
+
 		return "index";
 	}
 }
